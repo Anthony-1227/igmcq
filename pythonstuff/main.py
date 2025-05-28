20ef5bf0adf71bc353145e748e5bb7aa146b9bed
Anthony-1227 <aungwaimyo27@gmail.com>
2025-05-28 09:49:31 +0630

    hello

--- pythonstuff/main.py
+++ pythonstuff/main.py
@@ -1,4 +1,5 @@
 from pdf2image import convert_from_path
-pages = convert_from_path('test.pdf', 500, poppler_path=r"C:\Users\Ant\Documents\igmcq\pythonstuff\Release-24.08.0-0\poppler-24.08.0\Library\bin")
+pages = convert_from_path(r'C:\Users\Ant\Documents\igmcq\pythonstuff\test.pdf', 500, poppler_path=r"C:\poppler-24.08.0\Library\bin")
 for i in range(len(pages)):
         pages[i].save('image_name'+ str(i) +'.jpg', 'JPEG')
+        print(i)
